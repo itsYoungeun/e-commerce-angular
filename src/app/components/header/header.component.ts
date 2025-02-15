d0b0696ccd7f7eb5ddb3fe6bbf98bca4bf48ec08
Peter <youngeunpc@yahoo.com>
2025-02-14 22:08:18 -0300

    Reusable button component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-100 px-4 py-3 shadow-md">
-      <span>My Store</span>
+    <div class="flex justify-between items-center bg-slate-100 px-4 py-3 shadow-md">
+      <span class="text-xl">My Store</span>
+      <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()"/>
     </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
+  cart = signal('Cart');
+
+  showButtonClicked() {
+    console.log('button clicked')
+  }
 }
