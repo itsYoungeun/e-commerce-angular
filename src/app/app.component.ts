37a2194ee4e1cd990e54b9506b133d3737f09dfb
Peter <youngeunpc@yahoo.com>
2025-02-14 21:32:27 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header />
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'e-commerce';
+}
