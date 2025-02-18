481529264fcc2bffa147cadc5cffec24b9c9669c
Peter <youngeunpc@yahoo.com>
2025-02-18 19:55:49 -0300

    Route implementation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 import { CartService } from '../../services/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, RouterLink],
   template: `
     <div class="flex justify-between items-center bg-slate-100 px-4 py-3 shadow-md">
-      <span class="text-xl">My Store</span>
-      <app-primary-button [label]="'Cart (' + cartService.cart().length + ')'" />
+      <button class="text-xl" routerLink="/">My Store</button>
+      <app-primary-button 
+        [label]="'Cart (' + cartService.cart().length + ')'" 
+        routerLink = "/cart"
+      />
     </div>
   `,
   styles: ``
